f40b22fdc9537f7a57c1ef3bf3ff304248464ddf
pot-code <136290354@qq.com>
2023-07-09 17:20:54 +0800

    refactor: layout styles

--- src/layouts/SubLayout.vue
+++ src/layouts/SubLayout.vue
@@ -3,30 +3,13 @@ import { useRoute, useRouter } from 'vue-router'
 
 const route = useRoute()
 const router = useRouter()
-
-function goBack() {
-  router.go(-1)
-}
 </script>
 
 <template>
-  <van-nav-bar fixed :left-arrow="route.meta.backwards" :title="route.meta.title" @click-left="goBack" />
+  <van-nav-bar fixed :left-arrow="route.meta.backwards" :title="route.meta.title" @click-left="router.back()" />
   <div class="sub-view-container">
     <div class="view">
       <router-view />
     </div>
   </div>
 </template>
-
-<style scoped>
-.view-container {
-  height: 100%;
-  padding-top: 92px;
-  overflow: hidden;
-}
-
-.view {
-  height: 100%;
-  overflow: auto;
-}
-</style>
--- src/layouts/TabLayout.vue
+++ src/layouts/TabLayout.vue
@@ -4,29 +4,25 @@ import { ref } from 'vue'
 import { useRoute, useRouter } from 'vue-router'
 import { routeToTabItem } from './util'
 
-const route = useRoute()
-const router = useRouter()
-const active = ref(route.name?.toString())
-
 const tabItems = tabs.map(routeToTabItem)
 
-function goBack() {
-  router.go(-1)
-}
+const route = useRoute()
+const router = useRouter()
+const activeRoute = ref(route.name?.toString())
 </script>
 
 <template>
-  <van-nav-bar fixed :left-arrow="route.meta.backwards" :title="route.meta.title" @click-left="goBack" />
+  <van-nav-bar fixed :left-arrow="route.meta.backwards" :title="route.meta.title" @click-left="router.back()" />
   <div class="tab-view-container">
     <div class="view">
       <router-view />
     </div>
   </div>
-  <van-tabbar v-model="active">
+  <van-tabbar v-model="activeRoute">
     <van-tabbar-item v-for="item in tabItems" :key="item.label" :name="item.route" :to="{ name: item.route }">
       {{ item.label }}
       <template #icon="{ active }">
-        <svg-icon :name="`${item.icon}-${active ? 'active' : 'inactive'}`" />
+        <local-icon :name="`${item.icon}${active ? '-active' : ''}`" />
       </template>
     </van-tabbar-item>
   </van-tabbar>
