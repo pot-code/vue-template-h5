997a661b36127942ab70485ef72dd627142d9b50
pot-code <136290354@qq.com>
2023-09-18 23:39:44 +0800

    refactor: move HttpClient

--- src/core/http/index.ts
+++ src/core/http/index.ts
@@ -1,35 +1,6 @@
-import axios, { type AxiosInstance, type AxiosRequestConfig } from 'axios'
+import axios from 'axios'
+import HttpClient from './client'
 import { captureBusinessError, handleRejection } from './interceptors'
-
-class HttpClient {
-  constructor(private readonly client: AxiosInstance) {}
-
-  async get<T = any>(url: string, config?: AxiosRequestConfig<any>) {
-    return this.client.get<T>(url, config).then((res) => res.data)
-  }
-
-  async post<T = any>(url: string, data?: any, config?: AxiosRequestConfig<any>) {
-    return this.client.post<T>(url, data, config).then((res) => res.data)
-  }
-
-  async put<T = any>(url: string, data?: any, config?: AxiosRequestConfig<any>) {
-    return this.client.put<T>(url, data, config).then((res) => res.data)
-  }
-
-  async delete<T = any>(url: string, config?: AxiosRequestConfig<any>) {
-    return this.client.delete<T>(url, config).then((res) => res.data)
-  }
-
-  async request(method: string, url: string, config?: AxiosRequestConfig<any>) {
-    return this.client
-      .request({
-        method,
-        url,
-        ...config,
-      })
-      .then((res) => res.data)
-  }
-}
 
 export const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_PREFIX,
