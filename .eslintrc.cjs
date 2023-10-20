1fb930d9c980672a65effca1a8f740e28fdac1a1
pot-code <136290354@qq.com>
2023-10-20 23:41:27 +0800

    refactor: replace lodash with remeda

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,16 +4,15 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 module.exports = {
   root: true,
   extends: [
-    'plugin:vue/vue3-essential',
     'eslint:recommended',
+    'plugin:vue/vue3-essential',
     '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier/skip-formatting',
     '.eslintrc-auto-import.json',
   ],
   parserOptions: {
     ecmaVersion: 'latest',
   },
-  ignorePatterns: ['*.config.js','*.config.mjs'],
+  ignorePatterns: ['*.config.js', '*.config.mjs'],
   rules: {
     'vue/multi-word-component-names': 'off',
   },
